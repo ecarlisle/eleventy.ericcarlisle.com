81e23b66d169847ec32fe358aac9eb0f0bd2dc8a
Eric Carlisle <eric@ericcarlisle.com>
2020-03-08 22:30:13 -0400

    Move webpack folder.

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -75,7 +75,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              outputPath: '../fonts',
+              outputPath: 'fonts',
               name: '[name].[ext]'
             }
           }
